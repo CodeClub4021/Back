eeec1f56c4d3bf02253eebe0f43687e53857dca8
Sayin <sayin.ala81@gmail.com>
2023-12-25 13:49:17 +0330

    change url of mainpage

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GymFilterView
 
 urlpatterns = [
-    path('gyms/filter/', GymFilterView.as_view(), name='gym-filter'),
+    path('mainpage/gym_filter/', GymFilterView.as_view(), name='gym-filter'),
 ]
