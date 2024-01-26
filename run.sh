53a74c6e9fddf4c6492acf651961614116f7f02b
Sayin <sayin.ala81@gmail.com>
2024-01-26 21:33:16 +0330

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 pip install pipenv
 pipenv install django
 pipenv shell
@@ -8,9 +8,9 @@ pip install django-rest-authtoken
 pip install drf-yasg
 
 
-pip install djangorestframework-api-key
-pip install djangorestframework-simplejwt
-pip install django-cors-headers
+# pip install djangorestframework-api-key
+# pip install djangorestframework-simplejwt
+# pip install django-cors-headers
 
 
 python manage.py runserver 
