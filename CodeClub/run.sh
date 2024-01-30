843c15a3d443b0a7a7e78fde1433ec7f598509d3
Sayin <sayin.ala81@gmail.com>
2024-01-30 11:16:40 +0330

    Done

---
+++ CodeClub/run.sh
@@ -0,0 +1,16 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+pip install pipenv
+pipenv install django
+pipenv shell
+
+pip install djangorestframework
+pip install django-rest-authtoken
+pip install drf-yasg
+
+
+pip install djangorestframework-api-key
+pip install djangorestframework-simplejwt
+pip install django-cors-headers
+
+
+python manage.py runserver 
